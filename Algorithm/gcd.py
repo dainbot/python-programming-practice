eca8bf4ca8e2c8bad4a550f70f756d8f2493de6c
Dain <dain.park@my.jcu.edu.au>
2020-01-07 22:26:27 +0800

    070120 Algorithm exercise

---
+++ Algorithm/gcd.py
@@ -0,0 +1,16 @@
+# Find the greatest common denominator of two numbers.
+
+""" 1 For two integers a and b, where a > b, divide a by b
+    2 If the remainder, r, is 0, then stop: GCD is b
+    3 Otherwise, set a to b, b to r, and repeat at step 1 until r is 0 """
+
+def gcd(a, b):
+    while (b != 0):
+        t = a
+        a = b
+        b = t % b
+
+    return a
+
+print(gcd(60, 98))
+print(gcd(20, 8))
