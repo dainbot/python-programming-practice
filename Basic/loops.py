63660e007ea47111bd91a807afc28c338bc5b2eb
Dain <dain.park@my.jcu.edu.au>
2020-01-09 23:48:46 +0800

    090120 Loops practice, for and while loops, break and continue, enumerate

---
+++ Basic/loops.py
@@ -0,0 +1,26 @@
+def main():
+    x = 0
+
+    while (x<5):
+        print(x)
+        x = x+1
+
+    for x in range (5, 10):
+        print(x)
+
+    days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for d in days:
+        print (d)
+
+    for x in range(5,10):
+        #if (x==8): break
+        if (x % 2 == 0): continue
+        print(x)
+    
+# enumerate function gives numbers to each element
+    days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for i,d in enumerate(days):
+        print (i,d)
+
+if __name__ == "__main__":
+    main()
